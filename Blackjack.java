893093bef44141ce43091eebc78948b4d5686502
Rodrigo De Lascio <rodrigodelascio@gmail.com>
2024-05-14 17:02:31 +0100

    add random number + add cards drawn

--- Blackjack.java
+++ Blackjack.java
@@ -46,6 +46,12 @@ public class Blackjack {
      *   2. Returns a card.
      */
 
+    public static int drawRandomCard() {
+        int randomNumber = (int) (Math.random() * 13) + 1;
+        
+        return randomNumber;        
+    }
+
     /** Task 2 – make a function that returns a String drawing of the card.
      * Function name – cardString
      * @param cardNumber (int)
@@ -54,6 +60,133 @@ public class Blackjack {
      * Inside the function:
      *   1. Returns a String drawing of the card.
      */
+
+    public static String cardString(int cardNumber) {
+        
+        switch (cardNumber) {
+            case 1:
+                return
+                "   _____\n" +
+                "  |A _  |\n" + 
+                "  | ( ) |\n" +
+                "  |(_'_)|\n" +
+                "  |  |  |\n" +
+                "  |____V|\n";
+                
+            case 2:
+                return
+                "   _____\n"+              
+                "  |2    |\n"+ 
+                "  |  o  |\n"+
+                "  |     |\n"+
+                "  |  o  |\n"+
+                "  |____Z|\n";
+                
+            case 3:
+                return 
+                "   _____\n" +
+                "  |3    |\n"+
+                "  | o o |\n"+
+                "  |     |\n"+
+                "  |  o  |\n"+
+                "  |____E|\n";
+                
+            case 4:
+                return 
+                "   _____\n" +
+                "  |4    |\n"+
+                "  | o o |\n"+
+                "  |     |\n"+
+                "  | o o |\n"+
+                "  |____h|\n";
+
+            case 5:
+                return 
+                "   _____ \n" +
+                "  |5    |\n" +
+                "  | o o |\n" +
+                "  |  o  |\n" +
+                "  | o o |\n" +
+                "  |____S|\n";
+
+            case 6:
+                return 
+                "   _____ \n" +
+                "  |6    |\n" +
+                "  | o o |\n" +
+                "  | o o |\n" +
+                "  | o o |\n" +
+                "  |____6|\n";
+
+            case 7:
+                return 
+                "   _____ \n" +
+                "  |7    |\n" +
+                "  | o o |\n" +
+                "  |o o o|\n" +
+                "  | o o |\n" +
+                "  |____7|\n";
+
+            case 8:
+                return 
+                "   _____ \n" +
+                "  |8    |\n" +
+                "  |o o o|\n" +
+                "  | o o |\n" +
+                "  |o o o|\n" +
+                "  |____8|\n";
+
+            case 9:
+                return
+                "   _____ \n" +
+                "  |9    |\n" +
+                "  |o o o|\n" +
+                "  |o o o|\n" +
+                "  |o o o|\n" +
+                "  |____9|\n";
+                    
+            case 10:
+                return
+                "   _____ \n" +
+                "  |10  o|\n" +
+                "  |o o o|\n" +
+                "  |o o o|\n" +
+                "  |o o o|\n" +
+                "  |___10|\n";
+                
+            case 11:
+                return
+                "   _____\n" +
+                "  |J  ww|\n"+ 
+                "  | o {)|\n"+ 
+                "  |o o% |\n"+ 
+                "  | | % |\n"+ 
+                "  |__%%[|\n";
+
+            case 12:
+                return
+                "   _____\n" +
+                "  |Q  ww|\n"+ 
+                "  | o {(|\n"+ 
+                "  |o o%%|\n"+ 
+                "  | |%%%|\n"+ 
+                "  |_%%%O|\n";
+
+            case 13:
+                return
+                "   _____\n" +
+                "  |K  WW|\n"+ 
+                "  | o {)|\n"+ 
+                "  |o o%%|\n"+ 
+                "  | |%%%|\n"+ 
+                "  |_%%%>|\n";
+
+            default:
+                return "Houston, we have a problem.";
+        }
+
+    }
+
 
     public static String faceDown() {
         return
